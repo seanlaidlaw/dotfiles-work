066b3d958e1c21d2421959f1501e9d5d22989164
Sean LAIDLAW <seanlaidlaw95@gmail.com>
2019-04-30 15:00:17 +0100

    add zsh alias to fish

--- config.fish
+++ config.fish
@@ -1,1 +1,23 @@
-fish_vi_key_bindings
+# Set path
+set -gx PATH /anaconda3/bin /anaconda3/condabin /Users/sl31/homebrew/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin /usr/local/munki /opt/X11/bin
+set -x EDITOR vim
+set -x dotfiles /Users/sl31/Documents/dotfiles-work
+set -x PAGER bat
+
+abbr gd "cd $dotfiles"
+abbr pk "brew install"
+abbr t "tmux new"
+abbr tls "tmux ls"
+abbr ta "tmux a -t"
+abbr v "$EDITOR"
+alias c "$PAGER"
+abbr zz "omf reload"
+abbr py "python3"
+abbr py2 "python2.7"
+abbr pwdpb "pwd | pbcopy"
+alias mkdir "mkdir -pv"
+abbr chmox "chmod +x"
+abbr vv "cd $dotfiles; $EDITOR .vimrc"
+abbr vfsh "cd $dotfiles; $EDITOR config.fish"
+abbr stashme "git stash --include-untracked"
+abbr gdc "git diff --cached"
