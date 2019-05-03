d8774653769552542cff3dacc73c8cb5ca3ff304
Sean LAIDLAW <seanlaidlaw95@gmail.com>
2019-05-03 16:02:37 +0100

    reorganize folder and add script, so that stow can manage dotfiles (with use of setup.sh script)

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,7 @@
 # Set path
 set -gx PATH /anaconda3/bin /anaconda3/condabin /Users/sl31/homebrew/bin /usr/local/bin /Users/sl31/homebrew/Cellar/ /usr/bin /bin /usr/sbin /sbin /usr/local/munki /opt/X11/bin
-set -x EDITOR vim
-set -x dotfiles /Users/sl31/Documents/dotfiles-work
+set -x EDITOR nvim
+set -x dotfiles /Users/sl31/.dotfiles
 set -x PAGER bat
 
 abbr gd "cd $dotfiles"
@@ -18,6 +18,6 @@ abbr pwdpb "pwd | pbcopy"
 alias mkdir "mkdir -pv"
 abbr chmox "chmod +x"
 abbr vv "cd $dotfiles; $EDITOR .vimrc"
-abbr vfsh "cd $dotfiles; $EDITOR config.fish"
+abbr vfsh "cd $dotfiles; $EDITOR fish/.config/fish/config.fish"
 abbr stashme "git stash --include-untracked"
 abbr gdc "git diff --cached"
