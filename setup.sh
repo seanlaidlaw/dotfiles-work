30c6760678ed7748b61eb7d8d05ad9677eea9a5b
Sean LAIDLAW <seanlaidlaw95@gmail.com>
2019-05-03 16:02:37 +0100

    reorganize folder and add script, so that stow can manage dotfiles (with use of setup.sh script)

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+"""
+this bash script uses stow to make symlinks from the inside of every visible folder in directory of
+scripts to the parent folder, e.g. from cd ~/.dotfiles/ && stow vim, will be run which will symlink
+the vim/.vimrc to ~/.vimrc.
+
+to work as designed, please setup your dotfiles folder in the home directory
+"""
+cd ~/.dotfiles
+for folder in *; do
+    if [ -d "${folder}" ]; then
+        stow "${folder}"
+    fi
+done
