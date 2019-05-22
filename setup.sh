628a0986310139464f9a9ce83df8998d333e3db1
Sean LAIDLAW <seanlaidlaw95@gmail.com>
2019-05-23 00:47:33 +0100

    comment out setup.sh description

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-"""
-this bash script uses stow to make symlinks from the inside of every visible folder in directory of
-scripts to the parent folder, e.g. from cd ~/.dotfiles/ && stow vim, will be run which will symlink
-the vim/.vimrc to ~/.vimrc.
-
-to work as designed, please setup your dotfiles folder in the home directory
-"""
+# """
+# this bash script uses stow to make symlinks from the inside of every visible folder in directory of
+# scripts to the parent folder, e.g. from cd ~/.dotfiles/ && stow vim, will be run which will symlink
+# the vim/.vimrc to ~/.vimrc.
+#
+# to work as designed, please setup your dotfiles folder in the home directory
+# """
 cd ~/.dotfiles
 for folder in *; do
     if [ -d "${folder}" ]; then
