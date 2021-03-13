e66370f058fd0770878d64abfdaf139ed98876b5
Sean Laidlaw <seanlaidlaw95@gmail.com>
2021-03-13 20:23:37 +0000

    replace Vundle clone in setup.sh with vim-plug

--- setup.sh
+++ setup.sh
@@ -39,8 +39,8 @@ if [[ $(ls $HOME/.oh-my-zsh/custom/plugins | wc -l) -le 1 ]]; then
 fi
 
 
-# Vundle installation
-[ ! -d "$HOME/.vim/bundle/Vundle.vim" ] &&  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# vim-plug installation
+[ ! -f "$HOME/.local/share/nvim/site/autoload/plug.vim" ] && sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # TPM installation
 [ ! -d "$HOME/.tmux/plugins/tpm" ] && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
